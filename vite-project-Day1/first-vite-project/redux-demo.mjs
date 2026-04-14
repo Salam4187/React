e4d74cd4e121772b49a1a19da6262972add9d26c
Salam4187 <salamthulla@gmail.com>
2026-04-14 05:38:08 -0700

    day2-close added redux with sample

--- vite-project-Day1/first-vite-project/redux-demo.mjs
+++ vite-project-Day1/first-vite-project/redux-demo.mjs
@@ -1,21 +1,48 @@
-import {createStore} from 'redux'
+import { type } from 'os';
+import { createStore } from 'redux'
 
 
 //initial state
-const state={
+const state = {
     count: 5,
     message: "Hello redux"
 }
 
 //reducer
-const reducer= (currentState=state,action) =>{
-     
+const reducer = (currentState = state, action) => {
 
+    if (action.type === 'increment_counter') {
+        return {
+            ...currentState,
+            count: currentState.count + 1
+        }
+     }
 
-    return currentState;
+if (action.type === 'decrement_counter') {
+    return {
+        ...currentState,
+        count: currentState.count - 1
+    }
+}
+if (action.type === 'update_counter') {
+    return {
+        ...currentState,
+        count: action.value
+    }
+     }
+
+return currentState;
 }
 //store
-const store=createStore(reducer);
-console.log("state",store.getState());
+const store = createStore(reducer);
+console.log("state", store.getState());
 //subscribe
+
+store.subscribe(() => {
+    console.log("state", store.getState())
+})
 //disptach action
+
+store.dispatch({ type: "increment_counter" })
+store.dispatch({ type: "update_counter", value: 100 })
+store.dispatch({ type: "decrement_counter" })
